ec63f3897e5db8aa257ed7c93acdfb3ef6ec599e
klaudiajarebska <36846337+klaudiajarebska@users.noreply.github.com>
2018-04-05 11:31:20 +0200

    Add files via upload

---
+++ ludzie.js
@@ -0,0 +1,11 @@
+var ludzie = [
+
+	{ "imie":"Jan", "nazwisko":"Kowalski", "rok" : 1990 },
+	{ "imie":"Jan", "nazwisko":"Adamski", "rok" : 1992 },
+	{ "imie":"Alfred", "nazwisko":"Zajac", "rok" : 1970 },
+	{ "imie":"Zygmunt", "nazwisko":"Nowak", "rok" : 1988 },
+	{ "imie":"Andrzej", "nazwisko":"Malinowski", "rok" : 2000 },
+	{ "imie":"Jan", "nazwisko":"Dab", "rok" : 1991 },
+	{ "imie":"Michal", "nazwisko":"Czech", "rok" : 1991 },
+    { "imie":"Marcin", "nazwisko":"Xxx", "rok" : 1992 }
+]
